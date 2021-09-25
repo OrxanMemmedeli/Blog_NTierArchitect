12ea2b642065b47782b2f48c7bde8abcbb5c5c7c
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2021-09-25 13:02:50 +0400

    Using Inlude Method

---
+++ BusinessLayer/Concrete/BlogManager.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Concrete
+{
+    public class BlogManager : IBlogService
+    {
+        private readonly IBlogDal _blogDal;
+
+        public BlogManager(IBlogDal blogDal)
+        {
+            _blogDal = blogDal;
+        }
+
+        public void BlogAdd(Blog t)
+        {
+            _blogDal.Insert(t);
+        }
+
+        public void BlogDelete(Blog t)
+        {
+            _blogDal.Delete(t);
+        }
+
+        public void BlogUpdate(Blog t)
+        {
+            _blogDal.Update(t);
+        }
+
+        public List<Blog> GetAll()
+        {
+            return _blogDal.GetAll();
+        }
+
+        public List<Blog> GetAllWithRelationships()
+        {
+            return _blogDal.GetAllWithRelationships();
+        }
+
+        public Blog GetById(int id)
+        {
+            return _blogDal.GetById(id);
+        }
+    }
+}
--- DataAccessLayer/EntityFramework/EFBlogRepository.cs
+++ DataAccessLayer/EntityFramework/EFBlogRepository.cs
@@ -1,13 +1,23 @@
 ﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Context;
 using DataAccessLayer.Repositories;
 using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DataAccessLayer.EntityFramework
 {
     public class EFBlogRepository : GenericRepository<Blog>, IBlogDal
     {
+        public List<Blog> GetAllWithRelationships()
+        {
+            using (var context = new BlogContext())
+            {
+                return context.Blogs.Include(x => x.Category).ToList();
+            }
+        }
     }
 }
