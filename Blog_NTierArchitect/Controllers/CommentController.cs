1fb8ccacbc87848161391a674aa2cd831be586da
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2021-10-04 13:58:47 +0400

    ders 32 finish

---
+++ Blog_NTierArchitect/Controllers/CommentController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog_NTierArchitect.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly CommentManager _commentManager;
+
+        public CommentController()
+        {
+            _commentManager = new CommentManager(new EFCommentRepository());
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Blog_NTierArchitect/ViewComponents/BlogPageCategoryViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework.NewFolder;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog_NTierArchitect.ViewComponents
+{
+    public class BlogPageCategoryViewComponent : ViewComponent
+    {
+        private readonly CategoryManager _categoryManager;
+
+        public BlogPageCategoryViewComponent()
+        {
+            _categoryManager = new CategoryManager(new EFCategoryRepository());
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _categoryManager.GetAll();
+            return View(categories);
+        }
+    }
+}
