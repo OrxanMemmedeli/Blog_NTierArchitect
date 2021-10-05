11f6c942266654a31bae3c142b7d25bb9bc451ca
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2021-10-05 18:49:56 +0400

    ders 34

---
+++ BusinessLayer/Concrete/WriterManager.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Concrete
+{
+    public class WriterManager : IWriterService
+    {
+        private readonly IWriterDal _writerDal;
+
+        public WriterManager(IWriterDal writerDal)
+        {
+            _writerDal = writerDal;
+        }
+
+        public void WriterAdd(Writer t)
+        {
+            _writerDal.Insert(t);
+        }
+    }
+}
