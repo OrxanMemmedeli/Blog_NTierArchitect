d9204fe6baa742bb661b70a47283af904cd14d04
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2021-10-13 14:44:44 +0400

    footer section repair(part1)

--- Blog_NTierArchitect/Startup.cs
+++ Blog_NTierArchitect/Startup.cs
@@ -54,7 +54,7 @@ namespace Blog_NTierArchitect
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Blog}/{action=Index}/{id?}");
             });
         }
     }
