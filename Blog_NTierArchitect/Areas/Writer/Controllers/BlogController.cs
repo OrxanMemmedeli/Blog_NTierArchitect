6efc88c449ac146b8212a96c58e5c24f835f2c0e
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2021-10-30 14:00:38 +0400

    ders 55

--- Blog_NTierArchitect/Areas/Writer/Controllers/BlogController.cs
+++ Blog_NTierArchitect/Areas/Writer/Controllers/BlogController.cs
@@ -1,6 +1,11 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.Validations;
+using DataAccessLayer.Concrete.Context;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,15 +17,44 @@ namespace Blog_NTierArchitect.Areas.Writer.Controllers
     public class BlogController : Controller
     {
         private readonly BlogManager _blogManager;
+        private readonly BlogValidator _blogValidator;
+        private readonly BlogContext _context;
+
         public BlogController()
         {
             _blogManager = new BlogManager(new EFBlogRepository());
+            _blogValidator = new BlogValidator();
+            _context = new BlogContext();
         }
 
         public IActionResult Index()
         {
             var blogsByWriter = _blogManager.GetAllWithByWriter(1);
             return View(blogsByWriter);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewData["CategoryID"] = new SelectList(_context.Categories, "ID", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Blog blog)
+        {
+            ValidationResult results = _blogValidator.Validate(blog);
+            if (!results.IsValid)
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View(blog);
+            }
+            blog.WriterID = 1;
+            _blogManager.Add(blog);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
