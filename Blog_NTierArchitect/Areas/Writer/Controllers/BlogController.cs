70ae3db32c26422d2e7e559c4baa7e721f8eb1fc
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2021-11-01 11:51:07 +0400

    ders 60 (delete and update blogs)

--- Blog_NTierArchitect/Areas/Writer/Controllers/BlogController.cs
+++ Blog_NTierArchitect/Areas/Writer/Controllers/BlogController.cs
@@ -60,6 +60,13 @@ namespace Blog_NTierArchitect.Areas.Writer.Controllers
                 {
                     ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
                 }
+                List<SelectListItem> categories = (from x in _categoryManager.GetAll()
+                                              select new SelectListItem
+                                              {
+                                                  Text = x.Name,
+                                                  Value = x.ID.ToString()
+                                              }).ToList();
+                ViewData["CategoryID"] = categories;
                 return View(blog);
             }
             blog.WriterID = 1;
@@ -75,7 +82,61 @@ namespace Blog_NTierArchitect.Areas.Writer.Controllers
             }
 
             var blog = _blogManager.GetById((int)id);
+            if (blog == null)
+            {
+                return NotFound();
+
+            }
+
             _blogManager.Delete(blog);
+            TempData["DeleteBlog"] = "Blog silindi";
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            var blog = _blogManager.GetById((int)id);
+            if (blog == null)
+            {
+                return NotFound();
+
+            }
+
+            List<SelectListItem> items = (from x in _categoryManager.GetAll()
+                                          select new SelectListItem
+                                          {
+                                              Text = x.Name,
+                                              Value = x.ID.ToString()
+                                          }).ToList();
+            ViewData["CategoryID"] = items;
+            return View(blog);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Blog blog)
+        {
+            ValidationResult results = _blogValidator.Validate(blog);
+            if (!results.IsValid)
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                List<SelectListItem> categories = (from x in _categoryManager.GetAll()
+                                              select new SelectListItem
+                                              {
+                                                  Text = x.Name,
+                                                  Value = x.ID.ToString()
+                                              }).ToList();
+                ViewData["CategoryID"] = categories;
+                return View(blog);
+            }
+
+            _blogManager.Update(blog);
+            TempData["EditBlog"] = "Blog məlumatları yeniləndi.";
             return RedirectToAction(nameof(Index));
         }
     }
