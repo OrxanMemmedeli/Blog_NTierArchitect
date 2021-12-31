7182d89c60f61a3f64f6d73e13347a9a2540a8f0
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2021-12-31 21:36:04 +0400

    lesson 102 finish

---
+++ Blog_NTierArchitect/Areas/Admin/ViewComponents/Widgets/WidgetFourthRowViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog_NTierArchitect.Areas.Admin.ViewComponents.Widgets
+{
+    public class WidgetFourthRowViewComponent : ViewComponent
+    {
+        private readonly AdminManager _adminManager;
+        public WidgetFourthRowViewComponent()
+        {
+            _adminManager = new AdminManager(new EFAdminRepository());
+        }
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.Admin = _adminManager.GetById(1);
+            return View();
+        }
+    }
+}
