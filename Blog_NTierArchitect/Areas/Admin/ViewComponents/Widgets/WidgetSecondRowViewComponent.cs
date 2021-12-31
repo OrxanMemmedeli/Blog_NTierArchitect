b82c2f91f66308db26d827473d94310680ce55cb
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2021-12-31 20:24:56 +0400

    lesson 100 finish

---
+++ Blog_NTierArchitect/Areas/Admin/ViewComponents/Widgets/WidgetSecondRowViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog_NTierArchitect.Areas.Admin.ViewComponents.Widgets
+{
+    public class WidgetSecondRowViewComponent : ViewComponent
+    {
+        private readonly BlogManager _blogManager;
+        public WidgetSecondRowViewComponent()
+        {
+            _blogManager = new BlogManager(new EFBlogRepository());
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.LastBlog = _blogManager.GetAll().OrderByDescending(x => x.ID).Take(1).First();
+            return View();
+        }
+    }
+}
