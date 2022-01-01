045782472803c9625027b8d519b4a1ea5b6bcbcc
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2022-01-01 20:25:09 +0400

    googleChart error

---
+++ Blog_NTierArchitect/Areas/Admin/Models/CategoryChartViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog_NTierArchitect.Areas.Admin.Models
+{
+    public class CategoryChartViewModel
+    {
+        public string Category { get; set; }
+        public int BlogCount { get; set; }
+    }
+}
