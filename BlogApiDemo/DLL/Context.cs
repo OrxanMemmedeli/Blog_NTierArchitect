d108e807f221ff90feb3de3a86f991122ec38534
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2022-02-16 12:48:24 +0400

    finish lesson116

---
+++ BlogApiDemo/DLL/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApiDemo.DLL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=ILQAR\SQLEXPRESS01; Database=BlogAPIDemo; Integrated Security = true; MultipleActiveResultSets = True");
+            //optionsBuilder.UseSqlServer(@"Server=DESKTOP-TROAMS4; Database=BlogAPIDemo; Integrated Security = true; MultipleActiveResultSets = True");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
