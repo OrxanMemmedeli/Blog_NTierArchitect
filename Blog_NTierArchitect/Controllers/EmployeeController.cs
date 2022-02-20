a8142b98a64c18267b65706f6c664a8d041680f8
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2022-02-20 12:07:47 +0400

    finish lesson 120

---
+++ Blog_NTierArchitect/Controllers/EmployeeController.cs
@@ -0,0 +1,45 @@
+﻿using Blog_NTierArchitect.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog_NTierArchitect.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+
+        public async Task<IActionResult> Index()
+        {
+            var httpclient = new HttpClient();
+            var responseMessage = await httpclient.GetAsync("https://localhost:44358/api/Default");
+            var jsonString = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<EmployeeViewModel>>(jsonString);
+            return View(values);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(EmployeeViewModel employee)
+        {
+            var httpclient = new HttpClient();
+            var jsonEmployee = JsonConvert.SerializeObject(employee);
+            StringContent content = new StringContent(jsonEmployee, Encoding.UTF8, "application/json");
+            var responseMessage = await httpclient.PostAsync("https://localhost:44358/api/Default", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+    }
+}
---
+++ Blog_NTierArchitect/Models/EmployeeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog_NTierArchitect.Models
+{
+    public class EmployeeViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
