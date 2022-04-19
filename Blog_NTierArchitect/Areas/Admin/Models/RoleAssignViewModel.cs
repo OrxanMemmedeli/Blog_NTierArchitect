2336d96e534834fb8408ccdaad738bca50f0873e
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2022-04-19 14:08:20 +0400

    lesson 143  finish

---
+++ Blog_NTierArchitect/Areas/Admin/Models/RoleAssignViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog_NTierArchitect.Areas.Admin.Models
+{
+    public class RoleAssignViewModel
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public bool Exists { get; set; }
+    }
+}
