6af83ea80a1660ab20b4b3956b44342e00691365
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2022-04-28 15:17:52 +0400

    api context

--- BlogApiDemo/DLL/Context.cs
+++ BlogApiDemo/DLL/Context.cs
@@ -10,8 +10,9 @@ namespace BlogApiDemo.DLL
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=ILQAR\SQLEXPRESS01; Database=BlogAPIDemo; Integrated Security = true; MultipleActiveResultSets = True");
+            //optionsBuilder.UseSqlServer(@"Server=ILQAR\SQLEXPRESS01; Database=BlogAPIDemo; Integrated Security = true; MultipleActiveResultSets = True");
             //optionsBuilder.UseSqlServer(@"Server=DESKTOP-TROAMS4; Database=BlogAPIDemo; Integrated Security = true; MultipleActiveResultSets = True");
+            optionsBuilder.UseSqlServer(@"Server=161.97.166.102; Database=HilalDemoSecond; User Id=orxan; password=Ov!tBg@A2g2jA@Z; Trusted_Connection=False; MultipleActiveResultSets=true;");
         }
 
         public DbSet<Employee> Employees { get; set; }
